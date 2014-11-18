ce1963c270476ae4bc5395ad3ef4da69dd8e75b8
Travis Lane <travis.lane.osx@gmail.com>
2014-11-17 18:54:49 -0800

    Attempting to use the pedigree for cilk

--- src/parallel_pi_cilk.cpp
+++ src/parallel_pi_cilk.cpp
@@ -10,17 +10,18 @@
 
 #include <parallel_pi_cilk.h>
 #include <cilk/cilk.h>
+#include <cilk/cilk_api.h>
 #include <cilk/reducer_opadd.h>
+#include <ctime>
 
 	double
 cilk_estimate_pi(int32_t radius, uint64_t total_iterations)
 {
 	const uint64_t squared_radius = (uint64_t) radius * (uint64_t) radius;
 	cilk::reducer_opadd<uint64_t> total_inside(0);
-
+	
 	cilk_for(uint64_t i = 0; i < total_iterations; i++) {
-		std::random_device rd;
-		std::mt19937 gen(rd());
+		std::mt19937 gen(__cilkrts_get_pedigree());
 		std::uniform_int_distribution<int32_t> dist(-radius, radius);
 		x = dist(gen);
 		y = dist(gen);
